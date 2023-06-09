f6f53422401747e57565c100a880cca7539cc86e
Adolfo92 <adolfo92dev@gmail.com>
2023-06-09 07:53:58 -0400

    Add navbar items generator

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const contentDiv = document.querySelector("#content");
 const load = pageLoader;
 const navBar = document.createElement("nav");
 
+const navbarItems = ["home", "contact", "memes"];
+for (let i = 0; i < navbarItems.length; i++) {
+  const navItem = document.createElement("div");
+  navItem.classList.add(navbarItems[i]);
+  navItem.textContent =
+    navbarItems[i].charAt(0).toUpperCase() + navbarItems[i].slice(1);
+  navBar.appendChild(navItem);
+}
 contentDiv.appendChild(navBar);
-
 contentDiv.appendChild(load("home"));
