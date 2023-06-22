bb7a98f61366ba66dbb73d486e205e89bff5c266
Adolfo92 <adolfo92dev@gmail.com>
2023-06-22 18:50:18 -0400

    Agrega cartas en content.js usando el cardmaker

--- src/modules/content.js
+++ src/modules/content.js
@@ -5,7 +5,7 @@ import grave45 from "../img/bands/45Grave.jpeg";
 import eatYM from "../img/bands/eatYM.JPEG";
 import carnic from "../img/bands/Los_Carniceros.jpg";
 // ----- Tools
-import writeMethis from "./tools/articleWriter";
+
 import cardMaker from "./tools/cardMaker";
 
 const articles = {
@@ -13,23 +13,37 @@ const articles = {
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 };
+// Funcion para agregar a las cartas las clases respectivas a sus titulos, fotos y descripciones
+function bandCardInvoker(Title, Text, PhotoSRC) {
+  return cardMaker(
+    Title,
+    "bandTitle",
+    Text,
+    "bandText",
+    PhotoSRC,
+    "bandImage",
+    "bandCard"
+  );
+}
 
 function content() {
   const wrapper = document.createElement("div");
   wrapper.classList.add("wrapper");
   wrapper.classList.add("bandsWrapper");
   wrapper.appendChild(
-    cardMaker("Christian Death", articles.article1.text, christianDeath)
-  );
-  wrapper.appendChild(cardMaker("45Grave", articles.article1.text, grave45));
-  wrapper.appendChild(
-    cardMaker("Alien Sex Fiend", articles.article1.text, alienSF)
+    bandCardInvoker("Christian Death", articles.article1.text, christianDeath)
   );
   wrapper.appendChild(
-    cardMaker("Eat Your Make-up", articles.article1.text, eatYM)
+    bandCardInvoker("45Grave", articles.article1.text, grave45)
   );
   wrapper.appendChild(
-    cardMaker("Los Carniceros del norte", articles.article1.text, carnic)
+    bandCardInvoker("Alien Sex Fiend", articles.article1.text, alienSF)
+  );
+  wrapper.appendChild(
+    bandCardInvoker("Eat Your Make-up", articles.article1.text, eatYM)
+  );
+  wrapper.appendChild(
+    bandCardInvoker("Los Carniceros del norte", articles.article1.text, carnic)
   );
 
   console.log("content check");
