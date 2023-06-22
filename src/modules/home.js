bf90749bafcd2a026accaacec9b0562ca3e3f5c8
Adolfo92 <adolfo92dev@gmail.com>
2023-06-22 18:49:12 -0400

    Cambiame la tarjeta del home por hacedor de cartas

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,6 +1,9 @@
 import writeMethis from "./tools/articleWriter";
 import rozzPhoto from "../img/bands/RozzWilliamsHomepage.webp";
 
+// Import tools
+import cardMaker from "./tools/cardMaker";
+
 const article = {
   main: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 };
@@ -12,7 +15,7 @@ function homeLoader() {
   const title = document.createElement("h1");
   title.textContent = "Welcome";
   title.classList.add("title");
-
+  /*
   const description = document.createElement("div");
   description.classList.add("description");
   description.classList.add("card");
@@ -23,9 +26,20 @@ function homeLoader() {
   rozzWilliams.src = rozzPhoto;
   rozzWilliams.classList.add("homePhoto");
   description.appendChild(rozzWilliams);
-
+*/
   wrapper.appendChild(title);
-  wrapper.appendChild(description);
+  //wrapper.appendChild(description);
+  wrapper.appendChild(
+    cardMaker(
+      null,
+      null,
+      article.main,
+      "homeArticle",
+      rozzPhoto,
+      "homePhoto",
+      "description"
+    )
+  );
 
   console.log("Home.js check");
 
