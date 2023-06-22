16fec9e9590afafdcdf82ba59eddac614251b104
Adolfo92 <adolfo92dev@gmail.com>
2023-06-22 09:35:26 -0400

    Agrega funcion de escritura de parrafos

--- src/modules/home.js
+++ src/modules/home.js
@@ -2,7 +2,7 @@ import writeMethis from "./tools/articleWriter";
 import rozzPhoto from "../img/bands/RozzWilliamsHomepage.webp";
 
 const article = {
-  main: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  main: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
 };
 
 function homeLoader() {
--- src/modules/tools/articleWriter.js
+++ src/modules/tools/articleWriter.js
@@ -2,16 +2,36 @@ export default function writeMethis(text, identification) {
   const container = document.createElement("div");
   container.classList.add("writingCard");
 
-  const paragraph = document.createElement("p");
-
   if (identification) {
     container.classList.add(identification);
-    paragraph.classList.add(identification);
   }
 
-  paragraph.textContent = text;
+  function DETECT_LINE_BREAKS(someText) {
+    // Busca \n y me separas el texto en un array de parrafos
+    let paragraphArray = someText.split("\n");
 
-  container.appendChild(paragraph);
+    for (let i = 0; i < paragraphArray.length; i++) {
+      // Crea el parrafo
+      const paragraph = document.createElement("p");
+      // Agrega la clase
+      if (identification) {
+        paragraph.classList.add(identification);
+      }
+      // Agrega al parrafo el texto en la posicion del array actual
+      paragraph.textContent = paragraphArray[i];
+
+      //Metele el parrato al contenedor
+      container.appendChild(paragraph);
+
+      //Si no estamos en el final del array, introduce un salto de linea en el documento
+      if (i < paragraphArray.length - 1) {
+        const linebreak = document.createElement("br");
+        container.appendChild(linebreak);
+      }
+    }
+  }
+
+  DETECT_LINE_BREAKS(text);
 
   return container;
 }
