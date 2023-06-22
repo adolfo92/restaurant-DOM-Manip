74d2686ad78291923555a62054a7bb95846b4c6d
Adolfo92 <adolfo92dev@gmail.com>
2023-06-22 18:48:09 -0400

    Crea hoja de contacto con formulario sin action

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,6 +1,50 @@
 export default function contact() {
   const wrapper = document.createElement("div");
   wrapper.classList.add("wrapper");
+  wrapper.classList.add("contactWrapper");
+
+  const paragraph = document.createElement("p");
+  paragraph.textContent = "Website made by Adolfo Castro for The Odin project";
+  paragraph.classList.add("contactP");
+
+  const paragraph2 = document.createElement("p");
+  paragraph2.textContent = "Leave me a message!";
+  paragraph2.classList.add("contactP");
+  paragraph2.classList.add("messageInv");
+
+  const FORM = document.createElement("form");
+  FORM.action = "";
+  FORM.method = "post";
+  const fname = document.createElement("input");
+  fname.placeholder = "Your name";
+  fname.name = "name";
+  fname.type = "text";
+
+  const fmail = document.createElement("input");
+  fmail.name = "e-mail";
+  fmail.type = "email";
+  fmail.placeholder = "Your e-mail";
+
+  const ftext = document.createElement("textarea");
+  ftext.placeholder = "Your wonderful text";
+  ftext.name = "message";
+  ftext.cols = 50;
+  ftext.rows = 10;
+
+  const fbutton = document.createElement("button");
+  fbutton.value = "Submit";
+  fbutton.type = "submit";
+  fbutton.textContent = "Submit";
+  fbutton.addEventListener("click", (e) => e.preventDefault());
+
+  FORM.appendChild(fname);
+  FORM.appendChild(fmail);
+  FORM.appendChild(ftext);
+  FORM.appendChild(fbutton);
+
+  wrapper.appendChild(paragraph);
+  wrapper.appendChild(paragraph2);
+  wrapper.appendChild(FORM);
 
   console.log("contact check");
   return wrapper;
