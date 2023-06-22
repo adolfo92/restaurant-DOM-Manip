2040674bd12e71dd93ed7067cba18f0e9eb9c821
Adolfo92 <adolfo92dev@gmail.com>
2023-06-22 18:46:58 -0400

    Crea funcion hacedora de cartas para esta web

--- src/modules/tools/cardMaker.js
+++ src/modules/tools/cardMaker.js
@@ -1,20 +1,30 @@
 import writeMethis from "./articleWriter";
-export default function cardMaker(cardTitle, cardText, cardImageSrc) {
+export default function cardMaker(
+  cardTitle,
+  cardTitleID,
+  cardText,
+  cardTextID,
+  cardImageSrc,
+  cardImageID,
+  cardType
+) {
   const description = document.createElement("div");
-  description.classList.add("bandCard");
+  description.classList.add(cardType);
   description.classList.add("card");
 
-  const title = document.createElement("h2");
-  title.textContent = cardTitle;
-  title.classList.add("bandTitle");
+  if (cardTitle) {
+    const title = document.createElement("h2");
+    title.textContent = cardTitle;
+    title.classList.add(cardTitleID);
+    description.appendChild(title);
+  }
 
-  const cardArticle = writeMethis(cardText, "bandDesc");
+  const cardArticle = writeMethis(cardText, cardTextID);
 
   const cardImage = new Image();
   cardImage.src = cardImageSrc;
-  cardImage.classList.add("homePhoto");
+  cardImage.classList.add(cardImageID);
 
-  description.appendChild(title);
   description.appendChild(cardArticle);
   description.appendChild(cardImage);
 
